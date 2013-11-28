091870b5ec5bd2d75f9ec03665084cc943eea4eb
Fr1z2r <fr1z2rt@gmail.com>
2013-11-28 21:18:42 +0600

    Add task 1409

---
+++ 1409/1409/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers=Console.ReadLine().Split(' ');
+            var first = Convert.ToInt32(inputNumbers[0]);
+            var second = Convert.ToInt32(inputNumbers[1]);
+            var sum = first + second - 1;
+            Console.WriteLine((sum-first)+" "+(sum-second));
+        }
+    }
+}
---
+++ 1409/1409/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("1409")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1409")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b594aa67-2d17-4c36-9274-5dd7e96e7355")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
