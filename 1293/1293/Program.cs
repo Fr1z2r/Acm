4e9d649e43eff79a8f5b3b1d54c9d44627e32c13
Fr1z2r <fr1z2rt@gmail.com>
2013-11-28 21:25:29 +0600

    Add task 1293

---
+++ 1293/1293/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1293
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine().Split(' ');
+            var N = Convert.ToInt32(inputNumbers[0]);
+            var A = Convert.ToInt32(inputNumbers[1]);
+            var B = Convert.ToInt32(inputNumbers[2]);
+            var sumWeight = 2*N*A*B;
+            Console.WriteLine(sumWeight);
+        }
+    }
+}
