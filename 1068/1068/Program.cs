a8bb1fb1f3ae9c2530e8fd74cc05cbaf7d743cab
Fr1z2r <fr1z2rt@gmail.com>
2013-11-29 02:34:20 +0600

    Add task 1068

---
+++ 1068/1068/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _1068
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Enumerable.Range(number>0?1:number, Math.Abs(number - 1) + 1).Sum());
+        }
+    }
+}
