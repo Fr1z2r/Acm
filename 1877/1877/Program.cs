0f8d696a21ba62d1b2b5ca8218d27219a0a8c6a2
Fr1z2r <fr1z2rt@gmail.com>
2013-11-29 02:26:23 +0600

    Add task 1877

---
+++ 1877/1877/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1877
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code1 = Convert.ToInt32(Console.ReadLine());
+            var code2 = Convert.ToInt32(Console.ReadLine());
+            if(code1==0 || code1%2==0)Print(true);
+            else if(code2%2!=0)Print(true);
+            else Print(false);
+        }
+
+        static void Print(bool success)
+        {
+            Console.WriteLine(success ? "yes" : "no");
+        }
+    }
+}
