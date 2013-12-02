f0271c816f4fb5b9de86dd763fae8cbc08dffc06
Fr1z2r <fr1z2rt@gmail.com>
2013-12-03 02:59:37 +0600

    Add task 1126

---
+++ 1126/1126/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _1126
+{
+    class MagnetData
+    {
+        private int size;
+         
+        public MagnetData(int size)
+        {
+            this.size = size;
+        }
+
+        private int MaxOfList(int[] list, int pos)
+        {
+            var max = 0;
+            for (int i = pos; i < pos+size; i++)
+            {
+                if (list[i]>max)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+
+        public void FastResult(int[] values,int count)
+        {
+            for (int i = 0; i <= count-size; i++)
+            {
+                Console.WriteLine(MaxOfList(values,i));
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var memorySize = Int32.Parse(Console.ReadLine());
+            var magnedData = new MagnetData(memorySize);
+            int measurement;
+            var measurements = new int[26000];
+            var i = 0;
+            while ((measurement=Int32.Parse(Console.ReadLine()))!=-1)
+            {
+                measurements[i++]=(measurement);
+            }
+            magnedData.FastResult(measurements,i);
+        }
+    }
+}
